ea75213173fe0b7c260743d2a8646cc91361f270
ZichKoding <chriszichkocoding@gmail.com>
2024-05-05 09:24:45 -0400

    Added unittests for Apps|Descriptions and Summary

--- zichko_portfolio/portfolio/tests.py
+++ zichko_portfolio/portfolio/tests.py
@@ -35,3 +35,63 @@ class MissionStatementModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_mission = MissionStatement(pub_date=time)
         self.assertIs(future_mission.was_published_recently(), False)
+
+
+# Apps | Descriptions model tests
+class AppsDescriptionsModelTests(TestCase):
+    def test_was_published_recently_with_old_app(self):
+        """
+        was_published_recently() returns False for apps whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_app = AppsDescriptions(pub_date=time)
+        self.assertIs(old_app.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_app(self):
+        """
+        was_published_recently() returns True for apps whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_app = AppsDescriptions(pub_date=time)
+        self.assertIs(recent_app.was_published_recently(), True)
+
+    def test_was_published_recently_with_future_app(self):
+        """
+        was_published_recently() returns False for apps whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_app = AppsDescriptions(pub_date=time)
+        self.assertIs(future_app.was_published_recently(), False)
+
+
+# Summary model tests
+class SummmaryTests(TestCase):
+    def test_was_published_recently_with_old_summary(self):
+        """
+        was_published_recently() returns False for summaries whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_summary = Summary(pub_date=time)
+        self.assertIs(old_summary.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_summary(self):
+        """
+        was_published_recently() returns True for summaries whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_summary = Summary(pub_date=time)
+        self.assertIs(recent_summary.was_published_recently(), True)
+
+    def test_was_published_recently_with_future_summary(self):
+        """
+        was_published_recently() returns False for summaries whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_summary = Summary(pub_date=time)
+        self.assertIs(future_summary.was_published_recently(), False)
