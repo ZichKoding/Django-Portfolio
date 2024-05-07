d9e351ce8ff6e5a5959080d7456e8d2820a283a8
ZichKoding <chriszichkocoding@gmail.com>
2024-05-06 21:55:33 -0400

    basic header is now finished

---
+++ zichko_portfolio/portfolio/static/index.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var navList = document.getElementById('navList');
+    navList.classList.toggle('show');
+}
+
+// When menu-icon is clicked, toggle the menu
+// document.getElementById('menu-icon').addEventListener('click', function() {
+//         var navList = document.getElementById('navList');
+//         navList.classList.toggle('show');
+//     }
+// );
