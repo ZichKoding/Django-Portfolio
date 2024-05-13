b0bf0313b13d7106e37a1a81645709e2ed095cbb
ZichKoding <chriszichkocoding@gmail.com>
2024-05-12 21:44:06 -0400

    added initial outline for testing the methods in views.

--- zichko_portfolio/apps/tests.py
+++ zichko_portfolio/apps/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .views import SearchBarView, FitlerBoxView, AppsView
+
+# SearchBarView tests
+class SearchBarViewTests(TestCase):
+    def test_search_bar(self):
+        pass
+
+    def test_search_by_name(self):
+        pass
+
+    def test_search_by_category(self):
+        pass
+
+    def test_search_by_description(self):
+        pass
+
+
+# FitlerBoxView tests
+class FitlerBoxViewTests(TestCase):
+    def test_filter_box(self):
+        pass
+
+    def test_filter_by_category(self):
+        pass
+
+    def test_filter_by_publish_date(self):
+        pass
+
+    def test_filter_by_publish_date_range(self):
+        pass
+
+
+# AppsView tests
+class AppsViewTests(TestCase):
+    def test_get_apps(self):
+        pass
+
+    def test_get_seven_most_recent_active_apps(self):
+        pass
+
+    def test_get_pagination_of_apps(self):
+        pass
--- zichko_portfolio/apps/views.py
+++ zichko_portfolio/apps/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
-# Create your views here.
+from portfolio.models import AppsDescriptions
+
+
+class SearchBarView():
+    def search_bar():
+        pass
+
+    def search_by_name(request):
+        pass
+
+    def search_by_category(request):
+        pass
+
+    def search_by_description(request):
+        pass
+
+
+class FitlerBoxView():
+    def filter_box():
+        pass
+
+    def filter_by_category(request):
+        pass
+
+    def filter_by_publish_date(request):
+        pass
+
+    def filter_by_publish_date_range(request):
+        pass
+
+
+class AppsView():
+    def get_apps():
+        pass
+
+    def get_seven_most_recent_active_apps():
+        pass
+
+    def get_pagination_of_apps():
+        pass
