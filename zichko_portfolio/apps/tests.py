ef455be326dd74d496b91ec7e136ca91197f47a1
ZichKoding <chriszichkocoding@gmail.com>
2024-05-17 11:37:36 -0400

    Added all of the initial unit tests for get_default_apps

--- zichko_portfolio/apps/tests.py
+++ zichko_portfolio/apps/tests.py
@@ -154,6 +154,76 @@ class AppsViewTests(TestCase):
         self.assertEqual(response["current_page"], 1)
         self.assertEqual(response["total_pages"], 2)
 
+    def test_get_default_apps_success_less_than_seven(self):
+        '''
+        Testing the get_default_apps() method will return at
+        least one app when there are less than 7 apps in the database. 
+        Expected return is a dictionary with three keys:
+        {
+            "apps_description": all of the app information of the current page,
+            "current_page": this is a numeric value, and by default the value is one,
+            "total_pages": the length of pages that holds at least 7 apps per page
+        }
+        '''
+        create_apps(less_than_seven_apps)
+        apps_view = AppsView()
+        response = apps_view.get_default_apps()
+        self.assertEqual(len(response["apps_description"]), 3)
+        self.assertEqual(response["apps_description"][0].app_name, "Test App 1")
+        self.assertEqual(response["apps_description"][-1].app_name, "Test App 4")
+        self.assertEqual(response["current_page"], 1)
+        self.assertEqual(response["total_pages"], 1)
+
+    def test_get_default_apps_success_different_page(self):
+        '''
+        Testing the get_default_apps() method to make 
+        sure that it will return the 7 most recent active apps 
+        along with the pagination information. 
+        Expected return is a dictionary with three keys:
+        {
+            "apps_description": all of the app information of the current page,
+            "current_page": this is a numeric value, and by default the value is one,
+            "total_pages": the length of pages that holds at least 7 apps per page
+        }
+        '''
+        create_apps(less_than_seven_apps)
+        create_apps(more_apps)
+        apps_view = AppsView()
+        response = apps_view.get_default_apps(page_number=2)
+        self.assertEqual(len(response["apps_description"]), 2)
+        self.assertEqual(response["apps_description"][0].app_name, "More Apps 9")
+        self.assertEqual(response["apps_description"][6].app_name, "More Apps 10")
+        self.assertEqual(response["current_page"], 2)
+        self.assertEqual(response["total_pages"], 2)
+
+    def test_get_default_apps_success_no_apps(self):
+        '''
+        Testing the get_default_apps() method to make
+        sure that it will return a list of a default image and message
+        when there are no active apps in the database, or 
+        upon a database error.
+        Expected return is a dictionary with three keys:
+        {
+            "apps_description": [{
+                "app_image": "images/default_image.png",
+                "app_name": "No apps found",
+                "app_description": "No apps found at this time. Please check back later.",
+                "app_category": "No category found",
+                "app_url": "https://zichkoding.com",
+                "app_gh_url": "https://github.com/ZichKoding",
+                "pub_date": timezone.now(),
+                "active": True
+            }]
+            "current_page": this is a numeric value, and by default the value is one,
+            "total_pages": the length of pages that holds at least 7 apps per page
+        }
+        '''
+        apps_view = AppsView()
+        response = apps_view.get_default_apps()
+        self.assertEqual(len(response["apps_description"]), 1)
+        self.assertEqual(response["apps_description"][0].app_name, "No apps found at this time. Please check back later.")
+        self.assertEqual(response["current_page"], 1)
+        self.assertEqual(response["total_pages"], 1)
 
     def test_search_by_character_success(self):
         pass
