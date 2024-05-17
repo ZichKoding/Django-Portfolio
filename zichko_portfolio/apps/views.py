fdd4c137af8ab023737efd2ba1005d8ce9d89f97
ZichKoding <chriszichkocoding@gmail.com>
2024-05-17 00:03:34 -0400

    came up with a completely different design that will allow for one html page.

--- zichko_portfolio/apps/views.py
+++ zichko_portfolio/apps/views.py
@@ -3,38 +3,58 @@ from django.shortcuts import render
 from portfolio.models import AppsDescriptions
 
 
-class SearchBarView():
-    def search_by_name(request):
-        pass
-
-    def search_by_category(request):
-        pass
-
-
-class FitlerBoxView():
-    def filter_box():
-        pass
-
-    def filter_by_category(request):
-        pass
-
-    def filter_by_publish_date(request):
-        pass
-
-    def filter_by_publish_date_range(request):
-        pass
-
-
 class AppsView():
-    def get_apps():
+    def get_default_apps(self, page_number=1):
+        '''
+        This function needs to get the seven most recent apps. 
+        It, also, needs to return the pagination information with at 
+        7 apps per page. For example, object is returned with three keys:
+        apps_description - all of the app information of the current page
+        current_page - this is a numeric value, and by default the value is one. 
+        total_pages - the length of pages that holds at least 7 apps per page. 
+        '''
         pass
 
-    def get_seven_most_recent_active_apps():
+    def search_by_character(self, characters, page_number=1):
+        '''
+        This function needs to query the database based on characters
+        being searched and return the seven most recent based on characters
+        in search criteria. Also, needs to return the pagination information. 
+        For example, object is returned with three keys:
+        apps_description - all of the app information of the current page
+        current_page - this is a numeric value, and by default the value is one. 
+        total_pages - the length of pages that holds at least 7 apps per page. 
+        '''
         pass
 
-    def get_pagination_of_apps():
+    def filter_by_category(self, category, page_number=1):
+        '''
+        This function needs to query the database based on the category.
+        It needs to return a list of, at least, the 7 most recent apps 
+        from that category. Also, it needs to return the pagination.
+        For example, object is returned with three keys:
+        apps_description - all of the app information of the current page
+        current_page - this is a numeric value, and by default the value is one. 
+        total_pages - the length of pages that holds at least 7 apps per page. 
+        '''
         pass
 
+    def coordinator(self, url_params):
+        '''
+        This function will take the request url to splice, split, and parse 
+        the data to find the params. If there is no query param found (search or filter)
+        then extract the page number from the page_number query. Then call 
+        the method, get_default_apps() and return the response. 
 
+        If the query parameters contain search, extract the characters and 
+        page_number values, and call the method, search_by_character(), with the query
+        parameters and return the response. 
+
+        If the query parameters contain filter, extract the category and 
+        page_number values, and call the filter_by_category() with the 
+        query parameters. Then return the response. 
+        '''
+
+    
 def index(request):
     return render(request, "apps/index.html")
