1d09f9c6572d5303fcbd167ce08eac617fae9483
ZichKoding <chriszichkocoding@gmail.com>
2024-05-17 16:16:03 -0400

    added the functionality to search_by_character function.

--- zichko_portfolio/apps/views.py
+++ zichko_portfolio/apps/views.py
@@ -77,10 +77,41 @@ class AppsView():
         current_page - this is a numeric value, and by default the value is one. 
         total_pages - the length of pages that holds at least 7 apps per page. 
         '''
+        # Get the characters to search for in lowercase
+        lowercase_characters = characters.lower()
+        # Get the range of apps to display
+        if page_number > 1:
+            range_start = (page_number - 1) * 7
+            range_end = page_number * 7
+        else:
+            range_start = 0
+            range_end = 7
+        
+        # Get the apps or return default message if no apps are found
+        try:
+            # Get the total number of apps
+            total_apps = AppsDescriptions.objects.filter(app_name__icontains=lowercase_characters, active=True).count()
+            apps = AppsDescriptions.objects.filter(app_name__icontains=lowercase_characters, active=True).order_by("pub_date")[range_start:range_end]
+            if apps.count() == 0:
+                raise AppsDescriptions.DoesNotExist
+        except AppsDescriptions.DoesNotExist:
+            return {
+                "apps_description": self.default_response,
+                "current_page": 1,
+                "total_pages": 1,
+            }
+        
+        # Create an array of objects with app information
+        array_of_apps = list(apps.values())
+
+        # Get the current page and total pages
+        current_page = page_number
+        total_pages = total_apps // 7 + (total_apps % 7 > 0)
+
         return {
-            "apps_description": [],  # Replace with actual apps descriptions
-            "current_page": 1,  # Replace with actual current page
-            "total_pages": 1,  # Replace with actual total pages
+            "apps_description": array_of_apps,
+            "current_page": current_page,
+            "total_pages": total_pages,
         }
 
     def filter_by_category(self, category, page_number=1):
