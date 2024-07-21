dbb3e28f86faa452147bdf1f3db09981172381f4
ZichKoding <chriszichkocoding@gmail.com>
2024-07-21 12:18:15 -0400

    cleaning up some code and adding requirements.txt

--- zichko_portfolio/zichko_portfolio/settings.py
+++ zichko_portfolio/zichko_portfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%s6^)hp2*$*6l&6ooefw201ji*o9_m_4p)o)o3f*jxex1e4%n6'
+# Load .env file
+load_dotenv()
+
+# Get the secret key from the .env file
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
